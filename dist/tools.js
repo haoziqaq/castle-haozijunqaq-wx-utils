1342c51341441753d82694de96d1fbf6db6ccdd1
齐晧 <qihao@raybattle.com>
2019-03-21 10:13:35 +0800

    update version 1.0.31

--- dist/tools.js
+++ dist/tools.js
@@ -4,6 +4,53 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = {
+    /**
+     * 防抖函数
+     * @param method 事件触发的操作
+     * @param delay 多少毫秒内连续触发事件，不会执行
+     * @returns {Function}
+     */
+    $debounce: function $debounce(method, delay) {
+        var timer = null;
+        return function () {
+            var self = this;
+            var args = arguments;
+            timer && clearTimeout(timer);
+            timer = setTimeout(function () {
+                method.apply(self, args);
+            }, delay);
+        };
+    },
+
+
+    /**
+     * 节流函数
+     * @param method 事件触发的操作
+     * @param mustRunDelay 间隔多少毫秒需要触发一次事件
+     */
+    $throttle: function $throttle(method, mustRunDelay) {
+        var timer = void 0;
+        var args = arguments;
+        var start = void 0;
+        return function loop() {
+            var self = this;
+            var now = Date.now();
+            if (!start) {
+                start = now;
+            }
+            if (timer) {
+                clearTimeout(timer);
+            }
+            if (now - start >= mustRunDelay) {
+                method.apply(self, args);
+                start = now;
+            } else {
+                timer = setTimeout(function () {
+                    loop.apply(self, args);
+                }, 50);
+            }
+        };
+    },
     $showModal: function $showModal(title, content) {
         var showCancel = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
         var cancelText = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '取消';
--- src/tools.js
+++ src/tools.js
@@ -1,4 +1,51 @@
 export default {
+    /**
+     * 防抖函数
+     * @param method 事件触发的操作
+     * @param delay 多少毫秒内连续触发事件，不会执行
+     * @returns {Function}
+     */
+    $debounce(method, delay) {
+        let timer = null;
+        return function () {
+            let self = this;
+            let args = arguments;
+            timer && clearTimeout(timer);
+            timer = setTimeout(function () {
+                method.apply(self, args);
+            },delay);
+        }
+    },
+
+    /**
+     * 节流函数
+     * @param method 事件触发的操作
+     * @param mustRunDelay 间隔多少毫秒需要触发一次事件
+     */
+    $throttle(method, mustRunDelay) {
+        let timer;
+        let args = arguments;
+        let start;
+        return function loop() {
+            let self = this;
+            let now = Date.now();
+            if(!start){
+                start = now;
+            }
+            if(timer){
+                clearTimeout(timer);
+            }
+            if(now - start >= mustRunDelay){
+                method.apply(self, args);
+                start = now;
+            }else {
+                timer = setTimeout(function () {
+                    loop.apply(self, args);
+                }, 50);
+            }
+        }
+    },
+
     $showModal(title, content, showCancel = true, cancelText = '取消', cancelColor = '#000000', confirmText = '确定', confirmColor = '#576B95') {
         return new Promise((resolve, reject) => {
             wx.showModal({
