d56ccdcbe3ea96100b948dd28a72a7a61ed34328
chenkeyu <1140051355@qq.com>
2019-08-14 17:29:20 +0800

    获取当前月的最后一天

--- dist/tools.js
+++ dist/tools.js
@@ -524,5 +524,22 @@ exports.default = {
                 }
             });
         });
+    },
+    $getCurrentMonthLastDay: function $getCurrentMonthLastDay() {
+        var date = new Date();
+        var currentMonth = date.getMonth();
+        var nextMonth = ++currentMonth;
+        var nextMonthFirstDay = new Date(date.getFullYear(), nextMonth, 1);
+        var oneDay = 1000 * 60 * 60 * 24;
+        var lastTime = new Date(nextMonthFirstDay - oneDay);
+        var month = parseInt(lastTime.getMonth() + 1);
+        var day = lastTime.getDate();
+        if (month < 10) {
+            month = '0' + month;
+        }
+        if (day < 10) {
+            day = '0' + day;
+        }
+        return new Date(date.getFullYear() + '-' + month + '-' + day);
     }
 };
